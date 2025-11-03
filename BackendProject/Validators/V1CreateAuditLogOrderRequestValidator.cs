28197e9ea6c090d09c955d806caab5f18e6c2c30
Philipp Matuha <phil_th@mail.ru>
2025-11-03 15:22:24 +0300

    L2 T17 AuditLogOrder endpoint

---
+++ BackendProject/Validators/V1CreateAuditLogOrderRequestValidator.cs
@@ -0,0 +1,19 @@
+using FluentValidation;
+using Models.Dto.V1.Requests;
+
+namespace BackendProject.Validators;
+
+public class V1CreateAuditLogOrderRequestValidator : AbstractValidator<V1CreateAuditLogOrderRequest>
+{
+    public V1CreateAuditLogOrderRequestValidator()
+    {
+        RuleForEach(x => x.Orders).NotNull();
+        RuleForEach(x => x.Orders).ChildRules(order =>
+        {
+            order.RuleFor(o => o.OrderId).GreaterThan(0);
+            order.RuleFor(o => o.OrderItemId).GreaterThan(0);
+            order.RuleFor(o => o.CustomerId).GreaterThan(0);
+            order.RuleFor(o => o.OrderStatus).NotEmpty();
+        });
+    }
+}
