ee5e64620fdde746de847ce8d5eda660411e3389
Philipp Matuha <phil_th@mail.ru>
2025-11-08 11:56:15 +0300

    L2 PASSED + message bug fix

--- BackendProject/Validators/V1CreateAuditLogOrderRequestValidator.cs
+++ BackendProject/Validators/V1CreateAuditLogOrderRequestValidator.cs
@@ -7,10 +7,12 @@ public class V1CreateAuditLogOrderRequestValidator : AbstractValidator<V1AuditLo
 {
     public V1CreateAuditLogOrderRequestValidator()
     {
+        RuleFor(x => x.Orders).NotEmpty();
         RuleForEach(x => x.Orders).NotNull();
         RuleForEach(x => x.Orders).ChildRules(order =>
         {
-            //order.RuleFor(o => o.OrderId).GreaterThan(0);
+            order.RuleFor(o => o.OrderItemId).GreaterThan(0);
+            order.RuleFor(o => o.OrderId).GreaterThan(0);
             order.RuleFor(o => o.CustomerId).GreaterThan(0);
             order.RuleFor(o => o.OrderStatus).NotEmpty();
         });
